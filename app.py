c4ca206299740abfb545005c889efb69c2cec783
jsperry0807 <jsperry0807@gmail.com>
2026-01-07 12:50:04 -0500

    Update print statements in app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 print("Git/Github Demo")
 # some comment
-print("Hello from the remote repo")
-print("Hello from the local repo")
+print("Hello from the local repo 2")
+print("Hello from the remote repo 3")
+print("Hey local, you ain't so tough")
